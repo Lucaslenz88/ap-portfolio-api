55e30ed9a1a4498072a27ade15f01d6cc4852a42
Lucas Lenzetti <69922982+Lucaslenz88@users.noreply.github.com>
2022-07-31 00:54:56 -0300

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 @SpringBootApplication
 @RestController
 @RequestMapping(value="hello")
+@CrossOrigin
 public class App {
 
     public static void main(String[] args) {
